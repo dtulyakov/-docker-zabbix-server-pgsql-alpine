f7c478ff4ff9a0ed6366b056e37cdd0c09bb5bcd
Денис Туляков <d.tulyakov@ddos-guard.net>
2018-01-26 12:06:42 +0300

    На ветке master
     Начальный коммит

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build Status](https://travis-ci.org/dtulyakov/docker-zabbix-server-pgsql-alpine.svg?branch=master)](https://travis-ci.org/dtulyakov/docker-zabbix-server-pgsql-alpine)
+
+```BASH
+docker build --force-rm --no-cache --tag=dtulyakov/zabbix-server-pgsql-alpine:latest .
+
+```
+
---
+++ telegram_bot.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+TOKEN='Сюда пихаем токен'
+
+if [ $# -ne 3 ]; then echo 'Error! You must to define three params' && exit 1; fi
+
+CHAT_ID="$1"
+SUBJECT="$2"
+MESSAGE="$3"
+
+# Определяем emoji для темы сообщения
+if [ "$SUBJECT" = "PROBLEM" ];
+then
+        ICON="🔥"
+elif [ "$SUBJECT" = "OK" ];
+then
+        ICON="✅"
+else
+        ICON="⚠️"
+fi
+
+curl -ks --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${CHAT_ID}\",\"text\":\"$ICON ${SUBJECT}\n${MESSAGE}\"}" "https://api.telegram.org/bot${TOKEN}/sendMessage"
